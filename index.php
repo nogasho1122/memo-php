b69b0a112d6fc70dd2c672520d544c359cb35a92
nogasho1122 <exile1122sho@icloud.com>
2020-05-06 22:36:32 +0900

    sql実行

--- index.php
+++ index.php
@@ -19,7 +19,12 @@
 <h2>Practice</h2>
 <pre>
 <?php 
-// ここにプログラムを書く
+try{
+  $db = new PDO('mysql:dbname=mydb;host=127.0.0.1;port=8889;charset=utf8',
+  'root','root');
+} catch(PDOException $e){
+  echo 'DB接続エラー: ' . $e->getMessage();
+}
 ?>
 </pre>
 </main>
